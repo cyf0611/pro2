37699840b5e02d96392a5571d547c9b75b8eeb6d
cyf0611 <cao942789776@gmail.com>
2018-05-30 15:13:28 +0800

    1

--- data/common.inc.php
+++ data/common.inc.php
@@ -2,9 +2,9 @@
 //数据库连接信息
 $cfg_dbtype = 'mysql';
 $cfg_dbhost = 'localhost';
-$cfg_dbname = 'sql47_104_155_2';
-$cfg_dbuser = 'sql47_104_155_2';
-$cfg_dbpwd = 'ThGJzKNKyXFGSFfS';
+$cfg_dbname = 'www_zhendasm_co';
+$cfg_dbuser = 'www_zhendasm_co';
+$cfg_dbpwd = 'i6Q4Tnxbn5fQThZC';
 $cfg_dbprefix = 'dede_';
 $cfg_db_language = 'utf8';
 
